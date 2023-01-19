7076b562117164fa59bb54de8dd89677ca19d768
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2023-01-19 16:55:23 +0530

    Create Variables.php

---
+++ Variables.php
@@ -0,0 +1,13 @@
+<?php
+//echo "This is Anvesh Mangalam";
+//Variables are Containers for storing information 
+//Starts with a Dollars ($)
+$name = "Krishna";
+$Income=800000000;
+
+echo "This guy is $name and his Annual Income is $Income<br>";
+
+echo " $name is a Good Boy<br>";
+echo "$name he is the Real Super Hero<br>";
+
+?>
