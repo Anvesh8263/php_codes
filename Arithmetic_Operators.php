b6d8b5e3fe3ae685b393298e9c978ff036175a21
Anvesh8263 <anveshmangalam30@gmail.com>
2023-01-23 14:32:37 +0530

    Arithmetic

--- Arithmetic_Operators.php
+++ Arithmetic_Operators.php
@@ -3,15 +3,15 @@ $a = 125;
 $b = 56;
 
 echo  "Result of a + b is " . ($a+$b);
-
+echo"<br>";
 echo  "Result of a - b is " . ($a-$b);
-
+echo"<br>";
 echo  "Result of a * b is " . ($a*$b);
-
+echo"<br>";
 echo  "Result of a / b is " . ($a/$b);
-
+echo"<br>";
 echo  "Result of a**b is " . ($a**$b);
-
+echo"<br>";
 echo  "Result of a%b is " . ($a%$b);
-
+echo"<br>";
 ?>
---
+++ hh.php
@@ -0,0 +1,25 @@
+<?php
+  // $data = "Welcome Class" $data;
+  //// $data_new = 'Section-F' $data_new;
+  // var_dump($data);
+   //echo "<br>";
+   //var_dump($data_new);
+  //echo "Hello \n Welcome Class"$data;
+  //echo "Hello \n Welcome Class"$data_new;
+
+  echo 'Welcome Tutorial\'s php';
+
+
+  // echo "Hello <br> Welcome Class";
+
+
+
+
+//String
+
+//Single Qotation Strings Double Qotation Strings
+
+//Double qotation Strings -  
+//DOC Syntax - 1. Now Syntax String- Follow Single Qotd Syntax 2. Here Syntax String- follow Double Qoted Syntax 
+
+?>
