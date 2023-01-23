cbe3ab37263b157de7fb7a5dd0f949cd012854e6
Anvesh8263 <anveshmangalam30@gmail.com>
2023-01-23 15:23:37 +0530

    lab_work

---
+++ Lab_Work.php
@@ -0,0 +1,52 @@
+<?php
+  // $data = "Welcome Class" $data;
+  // $data_new = 'Section-F' $data_new;
+  // var_dump($data);
+   //echo "<br>";
+   //var_dump($data_new);
+  //echo "Hello \n Welcome Class"$data;
+  //echo "Hello \n Welcome Class"$data_new;
+
+  echo 'Welcome Tutorial\'s php';
+  echo |"<br>";
+ $data5 = <<< "mystring"
+ Hello class \n "welcome"
+ mystring;
+ echo $data
+
+  // echo "Hello <br> Welcome Class";
+
+
+
+echo chunk_split("Hello",4,':');
+echo convert_uuencode("Varun");
+echo convert_uudecode("%86MA");
+
+echo chr(32);
+//String
+
+//Single Qotation Strings Double Qotation Strings
+
+//Double qotation Strings -  
+//DOC Syntax - 1. Now Documentation  String- Follow Single Qotd Syntax 2. Here Documentation String- follow Double Qoted Syntax 
+$var_one = " php ";
+$var_new = chop();
+var_dump(var_one);
+var_dump(var_new);
+//String Functions
+
+1.addcslashes(Add character slashes)("Hello Classes","se") 
+2.addslashes()
+3.bin2hex()Converts a string of ASCII characters to hexadecimal values
+4.chop()remove the unwanted white spaces (IT WORKS ONLY IN THE RIGHT HAND)
+5.chunk_split()Splits a string into a series of smaller parts
+6.convert_uudecode()Decodes a uuencoded string
+7.convert_uuencode()Encodes a string using the uuencode algorithm
+8.chr()Returns a character from a specified ASCII value
+9.ord()Returns the ASCII value of the first character of a string
+10.print()Outputs one or more strings
+11.printf()Outputs a formatted string
+12.sprintf()Writes a formatted string to a variable
+13.echo()Outputs one or more strings
+
+?>
