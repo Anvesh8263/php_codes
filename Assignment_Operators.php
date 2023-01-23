80b8ae0c6088e23f51165ca4118fdde24c60cc52
Anvesh8263 <anveshmangalam30@gmail.com>
2023-01-23 14:08:51 +0530

    Assginment

---
+++ Assignment_Operators.php
@@ -0,0 +1,20 @@
+<?php
+$a = 95;
+$b = 5;
+
+$x= $a;
+echo "For the value is " . $a . "<br>";
+
+$a += 6;
+echo "For the value is " . $a . "<br>";
+
+
+$a -= 6;
+echo "For the value is " . $a . "<br>";
+
+$a *= 6;
+echo "For the value is " . $a . "<br>";
+
+$a /= 6;
+echo "For the value is " . $a . "<br>";
+?>
