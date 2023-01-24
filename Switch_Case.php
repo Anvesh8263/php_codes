92d55cfc9837d64df8b201e88aaa941f823932d2
Anvesh8263 <anveshmangalam30@gmail.com>
2023-01-24 17:54:19 +0530

    Switch_Case

---
+++ Switch_Case.php
@@ -0,0 +1,18 @@
+<?php
+$age=50;
+
+switch ($age) {
+    case 50:
+     echo " you are 50 years old";
+     break;
+     case 60:
+        echo " you are 60 years old";
+        break;
+        case 80:
+        echo " you are 80 years old";
+        break;
+    default:
+        echo "Your age is weird";
+        break;
+}
+?>
