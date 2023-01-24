b07228f7db24acc4b6ae1e2741a56fd39d30ce52
Anvesh8263 <anveshmangalam30@gmail.com>
2023-01-24 16:49:34 +0530

    if_else_conditionals

---
+++ if_else_conditionals.php
@@ -0,0 +1,30 @@
+<?php
+/*$a = 98;
+$b = 2;
+
+if($a>78){
+    echo " a is greater than 78";
+
+}
+else{
+    echo "a is not greater than 78";
+}*/
+
+$age = 16;
+//if else ladder
+if($age>18){
+ echo "You can Drink Tea , Alcohol as well as Water";
+}
+ elseif($age>14){
+ echo "You can Drink Tea and Water No Alcohol for you";
+ }
+ else{
+    echo "You can drink only water";
+ }
+
+ 
+ echo "<br>";
+ echo " Done";
+
+
+?>
