d7417ef52cddebd265b7bf19911cc9627c3e60b0
Anvesh8263 <anveshmangalam30@gmail.com>
2023-01-28 15:25:35 +0530

    For_Loops.php

---
+++ For_Loops.php
@@ -0,0 +1,21 @@
+<?php
+echo "This is for loop in Action";
+echo "<br>";
+
+for ($index=1; $index<6; $index++) { 
+    //for(initialization;condition;updation)
+    echo("The Number is $index <br>");
+    
+    //Avoid running into infinite loops 
+    //for($i=0; $i<87;){
+    //echo $i;
+    //}
+    echo "for loop is ended ";
+   
+}
+
+
+
+
+
+?>
