20009c40c4a50321e93ee49d30bcae5808ceaf87
Anvesh8263 <anveshmangalam30@gmail.com>
2023-01-28 16:20:13 +0530

    for_each

---
+++ for_each.php
@@ -0,0 +1,19 @@
+<?php
+/*echo " Welcome to the For_each Loop";
+echo "<br>";
+
+$arr = array("bananas","apples","Harpic","Bread");
+
+for ($i=0; $i < count($arr) ; $i++) { 
+    echo $arr[$i];
+    echo "<br>";
+}*/
+
+//Better way to do this 
+
+foreach ($arr as $value => $value) {
+    echo "$value <br>";
+}
+
+
+?>
