5b4ebecdc58f9ee1c2b60c73ef363fd471861f8a
Anvesh8263 <anveshmangalam30@gmail.com>
2023-01-30 09:17:30 +0530

    for_each

---
+++ Array.php
@@ -0,0 +1,8 @@
+<?php
+echo("Hello World");
+//Array can fetch only a particular integer not a full array
+//. opperator is used for concatination
+for ($index=0; $index <=2 ; $index++) {     
+    # code...
+}
+?>
--- for_each.php
+++ for_each.php
@@ -8,12 +8,14 @@ for ($i=0; $i < count($arr) ; $i++) {
     echo $arr[$i];
     echo "<br>";
 }*/
-
+//for each loop is used only for an array 
 //Better way to do this 
-
-foreach ($arr as $value => $value) {
+$arr = [10,20,30,40,50];
+foreach ($arr as $value){
     echo "$value <br>";
 }
-
-
+$arr1 = ["Shailendra","Ritesh","Devesh","Anvesh"];
+foreach ($arr1 as $key ){
+    echo "$key <br>";    
+}
 ?>
